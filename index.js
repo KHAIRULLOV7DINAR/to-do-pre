cc369200cd6039840941de5022dbae3ad28c3cd8
dinar <khairullovdinarr@gmail.com>
2026-02-25 00:34:18 +0300

    create_save_tasks

--- index.js
+++ index.js
@@ -12,17 +12,23 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 
-function loadTasks() {
+function loadTasks()
+{
+    if(localStorage.getItem("tasks"))
+    {
+        items = JSON.parse(localStorage.getItem("tasks"));
+    }
     return items;
 }
 
-function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
+function createItem(item)
+{
+    const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+    const textElement = clone.querySelector(".to-do__item-text");
+    const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+    const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+    const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
   textElement.textContent = item;
 
@@ -30,18 +36,41 @@ function createItem(item) {
 
 }
 
-function getTasksFromDOM() {
+function getTasksFromDOM()
+{
+    let itemsNamesElements = document.querySelectorAll(".to-do__item-text");
 
+    let tasks = [];
+
+    itemsNamesElements.forEach(item =>
+    {
+        tasks.push(item.textContent);
+    })
+
+    return tasks;
 }
 
-function saveTasks(tasks) {
-
+function saveTasks(tasks)
+{
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 
 items = loadTasks();
 
-items.forEach(item => {
+items.forEach(item =>
+{
     listElement.append(createItem(item));
 })
+
+formElement.addEventListener("submit", (event) =>
+{
+    event.preventDefault();
+
+    listElement.prepend(createItem(inputElement.value));
+
+    items = getTasksFromDOM();
+    saveTasks(items);
+    inputElement.value = "";
+});
 
